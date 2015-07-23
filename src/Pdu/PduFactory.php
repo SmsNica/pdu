1e56da697ce5c01ac14983347ead5614a849dbcb
Bruce Lampson <brucelampson@gmail.com>
2015-07-23 14:58:33 -0600

    Update PduFactory.php
    Fix typo

--- src/Pdu/PduFactory.php
+++ src/Pdu/PduFactory.php
@@ -18,7 +18,7 @@ class PduFactory {
     public static function decode($protocolDataUnit){
         if (false ==  preg_match('/^(?!>)^[A-F0-9]+\S[0-9A-F]$/',$protocolDataUnit)) {
             throw new InvalidArgumentException(sprintf(
-                '%s expects a valid as protocol data unit argument; received "%s"',
+                '%s expects a valid protocol data unit argument; received "%s"',
                 __METHOD__,
                 (is_object($protocolDataUnit) ? get_class($protocolDataUnit) : gettype($protocolDataUnit))
             ));
